5e96679a30329282bee5df8323ac4afc7d2a525b
Zeatrex <enesaksu9999@gmail.com>
2023-09-11 15:22:09 +0300

    BinaryConverter
    V1

--- binaryconverter/Form1.Designer.cs
+++ binaryconverter/Form1.Designer.cs
@@ -35,24 +35,30 @@
             Binary = new Label();
             textconvertbutton = new Button();
             Clearbutton = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // binaryconvertbutton
             // 
-            binaryconvertbutton.Location = new Point(323, 91);
+            binaryconvertbutton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            binaryconvertbutton.Location = new Point(12, 289);
             binaryconvertbutton.Name = "binaryconvertbutton";
-            binaryconvertbutton.Size = new Size(139, 40);
+            binaryconvertbutton.Size = new Size(271, 50);
             binaryconvertbutton.TabIndex = 1;
             binaryconvertbutton.Text = "Binary Convert";
             binaryconvertbutton.UseVisualStyleBackColor = true;
+            binaryconvertbutton.Click += binaryconvertbutton_Click;
             // 
             // binarybox
             // 
+            binarybox.AccessibleDescription = "only binary";
             binarybox.Location = new Point(12, 91);
             binarybox.Multiline = true;
             binarybox.Name = "binarybox";
             binarybox.Size = new Size(271, 138);
             binarybox.TabIndex = 2;
+            binarybox.TextChanged += binarybox_TextChanged;
             // 
             // textbox
             // 
@@ -61,50 +67,82 @@
             textbox.Name = "textbox";
             textbox.Size = new Size(271, 138);
             textbox.TabIndex = 3;
+            textbox.TextChanged += textbox_TextChanged;
             // 
             // Textt
             // 
             Textt.AutoSize = true;
-            Textt.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            Textt.Location = new Point(635, 25);
+            Textt.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            Textt.Location = new Point(615, 25);
             Textt.Name = "Textt";
-            Textt.Size = new Size(58, 35);
+            Textt.Size = new Size(66, 35);
             Textt.TabIndex = 4;
             Textt.Text = "Text";
             // 
             // Binary
             // 
             Binary.AutoSize = true;
-            Binary.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            Binary.Location = new Point(93, 25);
+            Binary.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            Binary.Location = new Point(88, 25);
             Binary.Name = "Binary";
-            Binary.Size = new Size(83, 35);
+            Binary.Size = new Size(90, 35);
             Binary.TabIndex = 5;
             Binary.Text = "Binary";
             // 
             // textconvertbutton
             // 
-            textconvertbutton.Location = new Point(323, 137);
+            textconvertbutton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textconvertbutton.Location = new Point(517, 289);
             textconvertbutton.Name = "textconvertbutton";
-            textconvertbutton.Size = new Size(139, 40);
+            textconvertbutton.Size = new Size(271, 50);
             textconvertbutton.TabIndex = 1;
             textconvertbutton.Text = "Text Convert";
             textconvertbutton.UseVisualStyleBackColor = true;
+            textconvertbutton.Click += textconvertbutton_Click;
             // 
             // Clearbutton
             // 
-            Clearbutton.Location = new Point(323, 183);
+            Clearbutton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Clearbutton.Location = new Point(309, 189);
             Clearbutton.Name = "Clearbutton";
-            Clearbutton.Size = new Size(139, 40);
+            Clearbutton.Size = new Size(185, 40);
             Clearbutton.TabIndex = 1;
             Clearbutton.Text = "Clear";
             Clearbutton.UseVisualStyleBackColor = true;
+            Clearbutton.Click += Clearbutton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(71, 232);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 23);
+            label1.TabIndex = 6;
+            label1.Text = "Only Binary here";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(595, 232);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 23);
+            label2.TabIndex = 6;
+            label2.Text = "Only Text here";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(Binary);
             Controls.Add(Textt);
             Controls.Add(textbox);
@@ -112,8 +150,10 @@
             Controls.Add(Clearbutton);
             Controls.Add(textconvertbutton);
             Controls.Add(binaryconvertbutton);
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
-            Text = "Form1";
+            Text = "BinaryTextConverter";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -127,5 +167,7 @@
         private Label Binary;
         private Button textconvertbutton;
         private Button Clearbutton;
+        private Label label1;
+        private Label label2;
     }
 }
--- binaryconverter/Form1.cs
+++ binaryconverter/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace binaryconverter
 {
     public partial class Form1 : Form
@@ -5,6 +7,113 @@ namespace binaryconverter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void binarybox_TextChanged(object sender, EventArgs e)
+        {
+            string binaryInput = binarybox.Text;
+            if (IsBinary(binaryInput))
+            {
+                string textOutput = BinaryToText(binaryInput);
+                textbox.Text = textOutput;
+            }
+            else
+            {
+                // Binary kutusuna geçerli bir deðer girildiðinde uyarý verme
+                if (!string.IsNullOrWhiteSpace(binaryInput))
+                {
+                    string textOutput = BinaryToText(binaryInput);
+                    textbox.Text = textOutput;
+                }
+                else
+                {
+                    textbox.Text = "";
+                }
+            }
+        }
+
+        private bool IsBinary(string binary)
+        {
+            foreach (char c in binary)
+            {
+                if (c != '0' && c != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void binaryconvertbutton_Click(object sender, EventArgs e)
+        {
+            string binaryInput = binarybox.Text;
+            string textOutput = BinaryToText(binaryInput);
+            textbox.Text = textOutput;
+        }
+
+        private void textconvertbutton_Click(object sender, EventArgs e)
+        {
+            string textInput = textbox.Text;
+            string binaryOutput = TextToBinary(textInput);
+            if (binaryOutput != null) // Geçerli bir dönüþüm sonucu varsa
+            {
+                binarybox.Text = binaryOutput;
+            }
+            else
+            {
+                // Metin kutusuna geçerli bir deðer girildiðinde uyarý verme
+                if (!string.IsNullOrWhiteSpace(textInput))
+                {
+                    MessageBox.Show("Geçersiz metin giriþi. Lütfen metin kutusuna geçerli bir metin girin.");
+                }
+                binarybox.Text = "";
+            }
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            binarybox.Text = "";
+            textbox.Text = "";
+        }
+        private void textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private string BinaryToText(string binary)
+        {
+            string[] binaryArray = binary.Split(' ');
+            byte[] bytes = new byte[binaryArray.Length];
+
+            try
+            {
+                for (int i = 0; i < binaryArray.Length; i++)
+                {
+                    bytes[i] = Convert.ToByte(binaryArray[i], 2);
+                }
+                return Encoding.UTF8.GetString(bytes);
+            }
+            catch (Exception ex)
+            {
+                // Binary kutusuna geçerli bir deðer girildiðinde uyarý verme
+                if (!string.IsNullOrWhiteSpace(binary))
+                {
+                    MessageBox.Show("Geçersiz binary giriþi: " + ex.Message);
+                }
+                return null;
+            }
+        }
+
+        private string TextToBinary(string text)
+        {
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            StringBuilder binaryStringBuilder = new StringBuilder();
+
+            foreach (byte b in textBytes)
+            {
+                binaryStringBuilder.Append(Convert.ToString(b, 2).PadLeft(8, '0') + " ");
+            }
+
+            return binaryStringBuilder.ToString().Trim();
         }
     }
 }
